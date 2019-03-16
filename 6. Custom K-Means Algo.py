456e2ac06fc0744ab07c503adf1aa2ca0bb23fbe
Purva Singh <singhpurva2906@gmail.com>
2019-03-16 16:44:44 +0530

    develop: Testing custom k-means algo against real-time data.

--- 6. Custom K-Means Algo.py
+++ 6. Custom K-Means Algo.py
@@ -2,8 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import style
 import warnings
-from math import sqrt
 from collections import Counter
+import pandas as pd
+import random
 style.use('fivethirtyeight')
 
 
@@ -24,16 +25,33 @@ def custom_k_nearest_neighbors(data,predict,k=3):
     vote_result = Counter(votes).most_common(1)[0][0]
     return vote_result
 
+df = pd.read_csv('breast-cancer-wisconsin.data.txt')
+df.replace('?',-99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+full_data = df.astype(float).values.tolist()
 
-dataset = {'k':[[1,2],[2,3],[3,1]], 'r':[[6,5],[7,7],[8,6]]}
-new_features = [5,7]
 
-for i in dataset:
-    for ii in dataset[i]:
-        plt.scatter(ii[0],ii[1],s=100,color=i)
+random.shuffle(full_data)
 
-plt.scatter(new_features[0], new_features[1], s=100)
+test_size = 0.2
+train_set = {2:[], 4:[]}
+test_set = {2:[], 4:[]}
+train_data = full_data[:-int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):]
 
-result = custom_k_nearest_neighbors(dataset, new_features)
-plt.scatter(new_features[0], new_features[1], s=100, color = result)
-plt.show()
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = custom_k_nearest_neighbors(train_set, data, k=5)
+        if group == vote:
+            correct += 1
+        total += 1
+print('Accuracy:', correct/total)
